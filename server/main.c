84f650d1978a01555dacf823b4c6bedf7c8e78fc
Vbeaufay <beaufaysvalentin@gmail.com>
2022-05-31 01:50:41 +0200

    server is working

--- server/main.c
+++ server/main.c
@@ -2,10 +2,21 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+char g_char;
 
 void handler(int sig)
 {
-	printf("%d\n", sig);
+	static size_t count;
+
+	g_char *= 2;
+	if (sig == SIGUSR2)
+		g_char += 1;
+	count++;
+	if (count == 8)
+	{
+		write(1, &g_char, 1);
+		count = 0;
+	}
 }
 
 void exit_end(int sig)
@@ -20,15 +31,16 @@ int	main()
 	pid_t pid;
 	struct sigaction sa;
 	struct sigaction end;
-
-	sa.sa_handler = handler;
-	end.sa_handler = exit_end;
+	
+	g_char = 0;
 	pid = getpid();
 	printf("%d\n", pid);
+	sa.sa_handler = handler;
+	end.sa_handler = exit_end;
 	sigaction(SIGUSR1, &sa, NULL);
 	sigaction(SIGUSR2, &sa, NULL);
 	sigaction(SIGINT, &end, NULL);
 	while(1)
-		sleep(3000);
+		pause();
 	return (EXIT_SUCCESS);
 }
